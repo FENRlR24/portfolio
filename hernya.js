e4ba29b5e2dac8d3f7993a3af12384fe78e7b86c
FENRlR24 <nikgnom29334@gmail.com>
2024-11-21 20:56:40 +0100

    4 news files

---
+++ hernya.js
@@ -0,0 +1,10 @@
+// Функция для переключения секций
+function openSection(sectionId) {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => {
+      section.style.display = 'none';
+  });
+  document.getElementById(sectionId).style.display = 'block';
+}
+
+  
